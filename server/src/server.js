9da10e1c473b29839192edcb6c08bac6f81fad45
danielace1 <danielsudharsan1805@gmail.com>
2025-12-11 16:31:11 +0530

    welcome email implemented

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,7 @@
 import express from "express";
-import "dotenv/config";
 import path from "path";
 
+import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -9,7 +9,7 @@ import messageRoutes from "./routes/message.route.js";
 const app = express();
 const __dirname = path.resolve();
 
-const PORT = process.env.PORT || 3000;
+const PORT = ENV.PORT || 3000;
 
 app.use(express.json());
 
@@ -17,7 +17,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 // Serve static files in production
-if (process.env.NODE_ENV === "production") {
+if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/dist")));
 }
 
