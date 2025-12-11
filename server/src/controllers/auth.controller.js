a94feaf583784a6aa10089b9f5c32f843a562bf5
danielace1 <danielsudharsan1805@gmail.com>
2025-12-11 15:37:07 +0530

    code refactor

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -27,17 +27,15 @@ export const signup = async (req, res) => {
       return res.status(400).json({ message: "Email already exists." });
     }
 
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     const newUser = new User({ fullName, email, password: hashedPassword });
 
     if (newUser) {
-      generateToken(newUser._id, res);
+      const savedUser = await newUser.save();
+      generateToken(savedUser._id, res);
 
-      await newUser.save();
-
-      res.status(201).json({
+      return res.status(201).json({
         _id: newUser._id,
         fullName: newUser.fullName,
         email: newUser.email,
