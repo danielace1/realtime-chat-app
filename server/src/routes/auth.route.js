058c61fb56643d89f4012b5c0744517d75ddfafb
danielace1 <danielsudharsan1805@gmail.com>
2025-12-12 17:47:58 +0530

    feat: update-profile api

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,10 +1,20 @@
 import express from "express";
-import { login, logout, signup } from "../controllers/auth.controller.js";
+import {
+  login,
+  logout,
+  signup,
+  updateProfile,
+  getProfile,
+} from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
+
+router.put("/update-profile", protectRoute, updateProfile);
+router.get("/check", protectRoute, getProfile);
 
 export default router;
