7246111456fd3ab0a16a6cc47b6b3af19c690a8b
danielace1 <danielsudharsan1805@gmail.com>
2025-12-16 19:22:01 +0530

    feat: message routes added

--- server/src/routes/message.route.js
+++ server/src/routes/message.route.js
@@ -1,10 +1,20 @@
 import express from "express";
+import {
+  getAllContacts,
+  getChatPartners,
+  getMessagesByUserId,
+  sendMessage,
+} from "../controllers/message.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
 
-router.post("/send", (req, res) => {
-  // Handle sending a message
-  res.send("Message sent");
-});
+router.use(arcjetProtection, protectRoute);
+
+router.get("/contacts", getAllContacts);
+router.get("/chats", getChatPartners);
+router.get("/:id", getMessagesByUserId);
+router.post("/send/:id", sendMessage);
 
 export default router;
